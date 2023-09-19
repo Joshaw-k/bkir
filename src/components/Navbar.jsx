9fbe60d54cf64d3ee4b3cf91257edc538893b40f
Joshaw-k <awinrin4bless@yahoo.com>
2023-09-19 22:31:00 +0100

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <header class="absolute md:max-w-2xl lg:max-w-4xl mx-auto left-0 top-0 right-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-transparent  border-b border-b-mainGold md:border-b-0 text-sm py-3 md:py-5 sm:py-0">
+    <header class="absolute md:max-w-2xl lg:max-w-4xl mx-auto left-0 top-0 right-0 flex flex-wrap sm:justify-start sm:flex-nowrap z-50 w-full bg-mainGold  border-b border-b-mainGold md:border-b-0 text-sm py-3 md:py-5 sm:py-0">
       <nav
-        class="relative max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
+        class="relative max-w-[85rem] w-full mx-auto sm:flex sm:items-center sm:justify-between sm:px-6 lg:px-8"
         aria-label="Global"
       >
-        <div class="flex items-center justify-between">
+        <div class="flex items-center justify-between px-4">
           <a
-            class="flex-none text-xl font-semibold text-mainGold"
+            class="flex-none text-xl font-semibold text-white"
             href="#"
             aria-label="Brand"
           >
@@ -18,13 +18,13 @@ const Navbar = () => {
           <div class="sm:hidden">
             <button
               type="button"
-              class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border border-mainGold font-medium text-mainGold shadow-sm align-middle hover:bg-white/[.1] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-mainGold transition-all text-sm"
+              class="hs-collapse-toggle p-2 inline-flex justify-center items-center gap-2 rounded-md border border-white font-medium text-mainGold shadow-sm align-middle hover:bg-white/[.1] hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-mainGold transition-all text-sm"
               data-hs-collapse="#navbar-collapse-with-animation"
               aria-controls="navbar-collapse-with-animation"
               aria-label="Toggle navigation"
             >
               <svg
-                class="hs-collapse-open:hidden w-4 h-4 text-mainGold"
+                class="hs-collapse-open:hidden w-4 h-4 text-white"
                 width="16"
                 height="16"
                 fill="currentColor"
@@ -36,7 +36,7 @@ const Navbar = () => {
                 />
               </svg>
               <svg
-                class="hs-collapse-open:block hidden text-mainGold w-4 h-4"
+                class="hs-collapse-open:block hidden text-white w-4 h-4"
                 width="16"
                 height="16"
                 fill="currentColor"
@@ -51,30 +51,21 @@ const Navbar = () => {
           id="navbar-collapse-with-animation"
           class="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow sm:block"
         >
-          <div class="flex flex-col gap-y-4 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
+          <div class="flex bg-white flex-col gap-y-4 px-3 py-6 gap-x-0 mt-5 sm:flex-row sm:items-center sm:justify-end sm:gap-y-0 sm:gap-x-7 sm:mt-0 sm:pl-7">
             <a
-              class="font-medium sm:py-6 text-mainGold hover:pl-3 hover:bg-white"
+              class="font-medium sm:py-6 text-mainGold hover:pl-3"
               href="#"
               aria-current="page"
             >
               Landing
             </a>
-            <a
-              class="font-medium text-white/[.8] hover:text-white sm:py-6 text-mainGold"
-              href="#"
-            >
+            <a class="font-medium /[.8] hover: sm:py-6 text-mainGold" href="#">
               Account
             </a>
-            <a
-              class="font-medium text-white/[.8] hover:text-white sm:py-6 text-mainGold"
-              href="#"
-            >
+            <a class="font-medium /[.8] hover: sm:py-6 text-mainGold" href="#">
               Work
             </a>
-            <a
-              class="font-medium text-white/[.8] hover:text-white sm:py-6 text-mainGold"
-              href="#"
-            >
+            <a class="font-medium /[.8] hover: sm:py-6 text-mainGold" href="#">
               Blog
             </a>
 
