9dcae3f12dbaae486ce09648931ac69bfd7a02f9
Joshaw-k <awinrin4bless@yahoo.com>
2023-09-20 00:46:10 +0100

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,88 +1,70 @@
 import React from "react";
-import about1 from "../assets/pexels-gustavo-fring-7156128.jpg";
+import {
+  FaRocket,
+  FaBriefcase,
+  FaUserAlt,
+  FaGraduationCap,
+} from "react-icons/fa";
+import christie from "../assets/christina.png";
 
 const About = () => {
   return (
-    <div className="py-36">
-      <div className="flex items-center max-w-5xl m-auto gap-x-16">
-        <div className="w-[40%] h-96 relative">
-          <div className="bg-[#E3CCAE] absolute -top-10 w-[26rem] h-96 -z-10 right-6"></div>
-          <div className="w-[100%] h-[100%]">
-            <img src={about1} className="w-[100%] h-[100%]" alt="" />
+    <div className="py-20 px-3">
+      <div>
+        <div className="flex gap-x-1 items-end">
+          <h1 className="font-bold text-5xl">HR</h1>
+          <div>
+            <p className="leading-3 font-light">
+              Welcome to <br />
+              <span className="text-2xl font-medium">CONSULT!</span>
+            </p>
           </div>
         </div>
-        <div className="w-[60%]">
-          <div className="max-w-lg flex flex-col gap-y-4 m-auto">
-            <h3 className="text-3xl font-semibold">
-              We create some things, Design for your success future.
-            </h3>
-            <p>
-              Lorem Ipsum is simply dumm of free available in market the way
-              printing and typesetting industry has been the industrys standard
-              dummy text ever.{" "}
-            </p>
+        <p className="my-6">
+          We believe in the value that our functions add to a business. We feel
+          that this specialist part of HR is often unrecognised for its
+          contribution to the profitability and success of a business.
+        </p>
+        <div>
+          <div className="border-b-2 flex gap-x-3 items-center py-2">
             <div>
-              <div className="flex gap-x-4 items-center">
-                <div className="w-3 h-3 bg-[#E3CCAE] rounded-full"></div>
-                <p className="font-semibold">
-                  Price of additional materials or parts (if needed){" "}
-                </p>
-              </div>
-              <div className="flex gap-x-4 items-center">
-                <div className="w-3 h-3 bg-[#E3CCAE] rounded-full"></div>
-                <p className="font-semibold">
-                  Transportation cost for carrying new materials/parts
-                </p>
-              </div>
-              <div className="flex gap-x-4 items-center">
-                <div className="w-3 h-3 bg-[#E3CCAE] rounded-full"></div>
-                <p className="font-semibold">
-                  You will get 100% money back offer.{" "}
-                </p>
-              </div>
+              <FaRocket className="text-gray-300 text-lg" />
             </div>
+            <p className="capitalize">
+              <span className="text-3xl font-medium">638</span> companies we
+              helped
+            </p>
+          </div>
+          <div className="border-b-2 flex gap-x-3 items-center py-2">
+            <div>
+              <FaBriefcase className="text-gray-300 text-lg" />
+            </div>
+            <p className="capitalize">
+              <span className="text-3xl font-medium">12</span> corporate
+              programs
+            </p>
+          </div>
+          <div className="border-b-2 flex gap-x-3 items-center py-2">
+            <div>
+              <FaGraduationCap className="text-gray-300 text-lg" />
+            </div>
+            <p className="capitalize">
+              <span className="text-3xl font-medium">28</span> training courses
+            </p>
+          </div>
+          <div className="border-b-2 flex gap-x-3 items-center py-2">
+            <div>
+              <FaUserAlt className="text-gray-300 text-lg" />
+            </div>
+            <p className="capitalize">
+              <span className="text-3xl font-medium">125</span> strategic
+              partners
+            </p>
           </div>
         </div>
       </div>
-      <div className="flex flex-row-reverse items-center max-w-5xl m-auto mt-32 gap-x-16">
-        <div className="w-[40%] h-96 relative">
-          <div className="bg-[#E3CCAE] absolute -top-10 w-[26rem] h-96 -z-10 -right-14"></div>
-          <div className="w-[100%] h-[100%]">
-            <img src={about1} className="w-[100%] h-[100%]" alt="" />
-          </div>
-        </div>
-        <div className="w-[60%]">
-          <div className="max-w-lg flex flex-col gap-y-4 m-auto">
-            <h3 className="text-3xl font-semibold">
-              We create some things, Design for your success future.
-            </h3>
-            <p>
-              Lorem Ipsum is simply dumm of free available in market the way
-              printing and typesetting industry has been the industrys standard
-              dummy text ever.{" "}
-            </p>
-            <div>
-              <div className="flex gap-x-4 items-center">
-                <div className="w-3 h-3 bg-[#E3CCAE] rounded-full"></div>
-                <p className="font-semibold">
-                  Price of additional materials or parts (if needed){" "}
-                </p>
-              </div>
-              <div className="flex gap-x-4 items-center">
-                <div className="w-3 h-3 bg-[#E3CCAE] rounded-full"></div>
-                <p className="font-semibold">
-                  Transportation cost for carrying new materials/parts
-                </p>
-              </div>
-              <div className="flex gap-x-4 items-center">
-                <div className="w-3 h-3 bg-[#E3CCAE] rounded-full"></div>
-                <p className="font-semibold">
-                  You will get 100% money back offer.{" "}
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div className="w-full">
+        <img src={christie} alt="christie.png" className="w-[80%]" />
       </div>
     </div>
   );
